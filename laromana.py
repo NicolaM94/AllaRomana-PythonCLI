ee4aca618c51300c9eb72f8202b704e049328e78
NicolaM94 <51529905+NicolaM94@users.noreply.github.com>
2021-08-10 15:40:44 +0200

    Manual update
    Manually updating from the replit.com project

--- laromana.py
+++ laromana.py
@@ -2,26 +2,27 @@ import csv, re, os
 from pathlib import Path
 
 
+
 def projectCreator():
     home = str(Path.home())
-    name = input("Come lo chiamiamo questo nuovo progetto?")
-    partecipants = input("Inserisci i nomi dei partecipanti, separali con la virgola: ")
+    name = input(">! Come lo chiamiamo questo nuovo progetto?")
+    partecipants = input(">! Inserisci i nomi dei partecipanti, separali con la virgola: ")
     partecipants = re.split(",",partecipants)
     print(partecipants)
     
     with open(home+"/"+name+".rmn","w") as file:
         file.write("Transazione,")
         for n in partecipants:
-            if partecipants.index(n) != len(partecipants):
+            if partecipants.index(n) != len(partecipants)+1:
               stringa = n+","
               file.write(stringa)
             else:
-              strnga = n+"\n"
               file.write(stringa)
-    print("Progetto creato!")
+        file.write("\n")
+    print(">! Progetto creato!")
 
 def projectLookup():
-    print("\nHo trovato questi progetti:")
+    print(">! Ho trovato questi progetti:")
     for file in os.listdir(Path.home()):
         if file.endswith(".rmn"):
             print(file[:-4])
@@ -29,44 +30,74 @@ def projectLookup():
 def projectAdder(project_name):
     try:
         with open(str(Path.home())+"/"+project_name+".rmn") as file:
-            print(f"Progetto {project_name} aperto!")
+            print(f">! Progetto {project_name} aperto!")
             reader = csv.reader(file,delimiter=",")
             head = next(reader)
             transactions = []
-            trans_name = input("Inserisci descrizione transazione: ")
+            trans_name = input(">! Inserisci descrizione transazione: ")
             transactions.append(trans_name)
-            for name in head[1:]:
-                insertion = input(f"Aggiungi movimento per {name} nella forma <spesa_effettiva;pagamento>")
+            for name in head[1:-1]:
+                insertion = input(f">! Aggiungi movimento per {name} nella forma <importo pagato|spesa effettiva>")
                 transactions.append(insertion)
-        print("Inserisco nel progetto!")
+        print(">! Inserisco nel progetto!")
         with open(str(Path.home())+"/"+project_name+".rmn","a") as file:
             for t in transactions:
                 s = t+","
                 file.write(s)
-            print("fatto!")
+            file.write("\n")
+            print(">! Fatto!")
 
     except Exception as error:
-        print("Qualcosa è andato storto",error)
+        print(">! Qualcosa è andato storto",error)
 
+def projectReader(project_name):
+  home = str(Path.home())+"/"+project_name+".rmn"
+  with open(home,"r") as file:
+    reader = file.readlines()
+    for l in reader:
+      print(l)
 
-print("\nBenvenuto in LaRomana. Cosa vuoi fare oggi?")
+def projectEval(project_name):
+  print(f">] Inizio a valutare il progetto '{project_name}'")
+  proj_file = open(str(Path.home())+"/"+project_name+".rmn","r")
+  
+
+print('''
+>[ ================================================= ]<
+    | Benvenuto in LaRomana. Cosa vuoi fare oggi? |
+>[ ================================================= ]<    ''')
 while True:
     print('''
-
-new - Crea nuovo progetto
-see - Vedi i progetti attivi
-add <name> - Aggiungi movimenti al progetto <name>
-cmd - Vedi comandi disponibili
-quit - Chiudi
+>] new - Crea nuovo progetto
+>] see - Vedi i progetti attivi
+>] see <name> - Vedi progetto <name>
+>] add <name> - Aggiungi movimenti al progetto <name>
+>] calculate <name> - Restituisce i risultati del progetto <name>
+>] clear - Pulisce lo schermo
+>] quit - Chiudi
 ''')
 
     ans = input()
 
     if ans == "new":
         projectCreator()
+    elif ans == "clear":
+      os.system('cls' if os.name == 'nt' else 'clear')
     elif ans == "see":
         projectLookup()
+    elif "see" in ans:
+        projectReader(ans[4:])
     elif "add" in ans:
         projectAdder(ans[4:])
+    elif "calculate" in ans:
+        projectEval(ans[10:])
     elif ans == "quit":
+        print(">! Arrivederci!")
         quit()
+    elif ans == "egg":
+        print('''
+,___,     ,___,
+[O.o]     [O.o]
+/)__)     /)__)
+-"--"------"--"----
+        ''')
