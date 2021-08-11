1d162ae372b1c6de7e44bb837c355b42e43d5943
NicolaM94 <51529905+NicolaM94@users.noreply.github.com>
2021-08-11 12:41:30 +0200

    Update parseandcalc.py

--- parseandcalc.py
+++ parseandcalc.py
@@ -1,10 +1,12 @@
 from os import read
-import re, csv
+from re import split
+import csv
 from pathlib import Path
 from time import sleep
 
 def calculator (project_name):
     
+    print(">]=======================================================[<")
     print(f">] Valutiamo il progetto '{project_name}' ...")
     proj_file = open(str(Path.home())+"/"+project_name+".rmn","r")
     reader = csv.reader(proj_file)
@@ -19,8 +21,8 @@ def calculator (project_name):
     for line in reader:
         pool = line[1:-1]
         for k,v in zip(header,pool):
-            partecipants[k]["Payed"] += float(re.split("\|",v)[0])
-            partecipants[k]["Spent"] += float(re.split("\|",v)[1])
+            partecipants[k]["Payed"] += float(split("\|",v)[0])
+            partecipants[k]["Spent"] += float(split("\|",v)[1])
 
     print(">] Calcolo dei delta per ciascun partecipante ...")
     sleep(1)
@@ -28,7 +30,7 @@ def calculator (project_name):
         partecipants[k]["Delta"] = partecipants[k]["Payed"] - partecipants[k]["Spent"]
         print("\t",k, partecipants[k])
 
-    print(">] Verifica anti-cheat ...")
+    print("\n>] Verifica anti-cheat ...")
     sleep(1)
     if sum([(partecipants[k]["Delta"]) for k in partecipants]) != 0:
       print("\t>! ... Whoops! Qualcuno ha barato qui!")
@@ -36,7 +38,7 @@ def calculator (project_name):
     else:
       print("\t>! ... a posto!")
 
-    print(">] Inizio a calcolare ...")
+    print("\n>] Inizio a calcolare ...")
 
     creditors = {}
     debitors = {}
@@ -51,3 +53,5 @@ def calculator (project_name):
     
     print(creditors)
     print(debitors)
+
+    print(">]=======================================================[<")
